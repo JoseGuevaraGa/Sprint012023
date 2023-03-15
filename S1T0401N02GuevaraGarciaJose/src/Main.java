849822fed7b72c5db9d78d66f4a9b7c95bba177e
Jose Guevara <jose.guevara.ga@gmail.com>
2023-03-15 17:39:20 +0100

    Tasca 4 correcciones, Entrega Nivel 2

---
+++ S1T0401N02GuevaraGarciaJose/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+public class Main {
+    public static void main(String[] args) {
+
+        String cadena = "Mordor";
+        int resultado = 0;
+
+        resultado = Texto.verLongitud(cadena);
+
+        System.out.println("Longitud del texto: " + resultado );
+
+
+
+    }
+
+
+}
